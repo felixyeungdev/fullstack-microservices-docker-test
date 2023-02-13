9c5f8cd1b570df67f9d68f58c94fc97c3226798c
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2023-02-13 21:41:21 +0000

    duplicate microservices

---
+++ backend/services/shops/main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+import uvicorn
+import os
+
+PORT = os.environ.get("PORT", "8000")
+HOST = os.environ.get("HOST", "0.0.0.0")
+PORT = int(PORT)
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello World", "success": True}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host=HOST, port=PORT)
