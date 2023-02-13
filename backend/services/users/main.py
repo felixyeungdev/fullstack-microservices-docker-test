8b63404aac83e63a533f20f112f3886d7a854e61
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2023-02-13 21:33:40 +0000

    services

--- backend/services/users/main.py
+++ backend/services/users/main.py
@@ -2,8 +2,9 @@ from fastapi import FastAPI
 import uvicorn
 import os
 
-PORT = os.environ.get("PORT", 8000)
+PORT = os.environ.get("PORT", "8000")
 HOST = os.environ.get("HOST", "0.0.0.0")
+PORT = int(PORT)
 
 app = FastAPI()
 
